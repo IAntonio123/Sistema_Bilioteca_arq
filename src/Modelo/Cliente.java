2fe2c2c0033ae92d81aae2aeed2eb3c45b06e819
IAntonio123 <antoniomayhuay@gmail.com>
2023-06-25 18:39:37 -0500

    actualizando ...

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -9,5 +9,71 @@ package Modelo;
  * @author Antonio
  */
 public class Cliente {
+    private String idcli;
+    private String nombrecli;
+    private String apellicli;
+    private String correocli;
+    private String direccioncli;
+    private int telefonocli;
+    private String dnicli;
+
+    public Cliente() {
+    }
+
+    public String getIdcli() {
+        return idcli;
+    }
+
+    public void setIdcli(String idcli) {
+        this.idcli = idcli;
+    }
+
+    public String getNombrecli() {
+        return nombrecli;
+    }
+
+    public void setNombrecli(String nombrecli) {
+        this.nombrecli = nombrecli;
+    }
+
+    public String getApellicli() {
+        return apellicli;
+    }
+
+    public void setApellicli(String apellicli) {
+        this.apellicli = apellicli;
+    }
+
+    public String getCorreocli() {
+        return correocli;
+    }
+
+    public void setCorreocli(String correocli) {
+        this.correocli = correocli;
+    }
+
+    public String getDireccioncli() {
+        return direccioncli;
+    }
+
+    public void setDireccioncli(String direccioncli) {
+        this.direccioncli = direccioncli;
+    }
+
+    public int getTelefonocli() {
+        return telefonocli;
+    }
+
+    public void setTelefonocli(int telefonocli) {
+        this.telefonocli = telefonocli;
+    }
+
+    public String getDnicli() {
+        return dnicli;
+    }
+
+    public void setDnicli(String dnicli) {
+        this.dnicli = dnicli;
+    }
     
 }
